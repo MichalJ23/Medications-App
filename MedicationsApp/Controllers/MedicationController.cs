e50b02d1b290f9cefce869aa64419d933c099455
MichalJ <ul0260196@edu.uni.lodz.pl>
2023-03-06 19:14:42 +0100

    start of the project

---
+++ MedicationsApp/Controllers/MedicationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MedicationsApp.Models;
+
+
+namespace MedicationsApp.Controllers
+{
+    public class MedicationController : Controller
+    {
+        private static List<MedicationViewModel> meds = new List<MedicationViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(meds);
+        }
+
+        public IActionResult Create()
+        {
+            var med = new MedicationViewModel();
+            return View(med);
+        }
+
+        public IActionResult CreateMed(MedicationViewModel medicationViewModel)
+        {
+            meds.Add(medicationViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MedicationsApp/Models/MedicationViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MedicationsApp.Models
+{
+    public class MedicationViewModel
+    {
+        public static int IdCounter = 0;
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Portion { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
